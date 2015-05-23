3e32ebac95113e3cc5996a8f82d267f268b9559f
menrotProg <menrot.prog@gmail.com>
2015-05-23 10:08:26 +0300

    Fixed the regex and added documentation
    1. The regex handles now "()"
    2. Added CodeBook.md
    3.  Edited and completed the readme.md file

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # GettingandCleaningData-Project
 This repo is for the course project of Getting and Cleaning Data
+
+Processed Human Activity Recognition Using Smartphones Data Set 
+
+
+Abstract: Based on Human Activity Recognition database built from the recordings of 30 subjects performing activities of daily living (ADL) while carrying a waist-mounted smartphone with embedded inertial sensors (reference - http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones), the script run_analysis.R creates a dataset including the average for each subject and each activity of mean and std for each feature.
+
+
+The files in the repo are:
+	Readme.md
+	run_Analysis.R - the script file using the refernce dataset and creating the "tidy.txt" dataset.
+	CodeBook.md - describing the resulting dataset
+	tidy.txt - the resulting dataset
+
+How to run the run_analysis.R script
+	The script assumes
+		1) in is run from the working directory
+		2) the working directory includes the original data set in a folder "UCI HAR Dataset"
+	The script creates in the working directory the resulting dataset with the name "tidy.txt"
+	
+	
+		
+
+
+
+Data Set Characteristics:  
+
+Number of Instances:
+
+180
+
+Number of Attributes:
+
+68
+
+Data Set Information:
+
+The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. 
+
+
+Attribute Information:
+
+For each record in the dataset it is provided: 
+- An identifier of the subject who carried out the experiment.
+- Its activity label. 
+- A 66-feature vector having the average of the selected (MEAN and STD)features. A detailed list can be found in the CodeBook.MD file.
+  
+Processing
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,24 @@
-rm(list=ls())
-setwd("C:\\Users\\Menashe\\Google Drive\\Coursera\\Working Directory")
+###########################################################################################
+##
+## Script to process the original dataset -
+##    http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+##    and create a "tidy.txt" as desribed in the readme.md file
+##
+## Assumptions:
+##    Working directory is set prior to running the script
+##    The working directory includes the original dataset in folder "UCI HAR Dataset" 
+##
+###########################################################################################
 
-library(plyr)
+rm(list=ls())
+
+## setwd("~/../Google Drive/Coursera/Working Directory")
+
 library(dplyr)
 
-## To use descriptive activity names to name the activities in the data set
+##
+## 
+## To use descriptive activity names to name the activities in the data set (step #3)
 ##	prepare the data to change the numeric value of Activity to a descriptive Factor
 
 ActivityLabels <- read.table("UCI HAR Dataset/activity_labels.txt", header = FALSE, stringsAsFactors=FALSE)
@@ -70,29 +84,29 @@ MergedDF <- rbind(TrainDF, TestDF)
 #########################################################
 
 ## Name the Subject and Activity columns
-cols1 <- data.frame( V1=c(0,0),  V2=c("Subject","Activity"))
+colslabel1 <- data.frame( V1=c(0,0),  V2=c("Subject","Activity"))
 
 ## Read the columns name from the features file
-cols2 <- read.table("UCI HAR Dataset/features.txt", header = FALSE)
+colslabel2 <- read.table("UCI HAR Dataset/features.txt", header = FALSE)
 
 ##Create the column name list
-cols <-rbind(cols1,cols2)
+colslabel <-rbind(colslabel1,colslabel2)
 
 ## Assign the column names
-colnames(MergedDF)<-cols[,2]
+colnames(MergedDF)<-colslabel[,2]
 
 ###################################################
 ##
-## Extract only the mean and std columns - Step #2
+## Extract only the mean() and std() columns - Step #2
 ##
 ###################################################
 
-logicalVec1 <- grepl("mean", names(MergedDF), ignore.case=TRUE)
-logicalVec2 <- grepl("std", names(MergedDF), ignore.case=TRUE)
+logicalVec1 <- grepl("mean\\(\\)", names(MergedDF), ignore.case=TRUE)
+logicalVec2 <- grepl("std\\(\\)", names(MergedDF), ignore.case=TRUE)
 
 logicalVec <- logicalVec1 | logicalVec2
 
-## Keep the Subject and Activity
+## Keep the Subject and Activity columns
 logicalVec[1] <- TRUE
 logicalVec[2] <- TRUE
 
@@ -104,6 +118,7 @@ ExtractedDF <- MergedDF[, logicalVec]
 ## Calculate the average for each Sunject and each Activity - total of 180 rows 
 ##  (30 subjects 8 6 Activities)
 ##  This is Step #5
+##
 ##############################################################
 
 
@@ -114,6 +129,4 @@ tidy <- ddply(ExtractedDF, .(Subject, Activity), colwise(mean))
 ###########################
 
 write.table(tidy, file="tidy.txt", row.name=FALSE)
-
-
 
